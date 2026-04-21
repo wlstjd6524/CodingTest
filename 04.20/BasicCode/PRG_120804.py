56086e5e2ca4c158ce4f9f9d0ad9b2afeeebe3e1
wlstjd6524 <wlstjd6524@naver.com>
2026-04-21 22:22:50 +0900

    feat: PRG 120804 - 두 수의 곱 구하기

---
+++ 04.20/BasicCode/PRG_120804.py
@@ -0,0 +1,9 @@
+'''
+[프로그래머스 입문코드 다루기]
+
+프로그래머스 120804: 두 수의 곱 구하기
+URL : https://school.programmers.co.kr/learn/courses/30/lessons/120804
+'''
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
