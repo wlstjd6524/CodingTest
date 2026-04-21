558d8b5939ec5a1e6a123672ac6ebdae75a15bfe
wlstjd6524 <wlstjd6524@naver.com>
2026-04-21 22:22:30 +0900

    feat: PRG 120803 - 두 수의 차 구하기

---
+++ 04.20/BasicCode/PRG_120803.py
@@ -0,0 +1,9 @@
+'''
+[프로그래머스 입문코드 다루기]
+
+프로그래머스 120803: 두 수의 차 구하기
+URL : https://school.programmers.co.kr/learn/courses/30/lessons/120803
+'''
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer
