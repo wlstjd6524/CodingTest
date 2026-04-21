720f58b77ad8f89d3f2a31dd8ae7d3f6b3ad1605
wlstjd6524 <wlstjd6524@naver.com>
2026-04-21 22:22:01 +0900

    feat: PRG 120802 - 두 수의 합 구하기

---
+++ 04.20/BasicCode/PRG_120802.py
@@ -0,0 +1,9 @@
+'''
+[프로그래머스 입문코드 다루기]
+
+프로그래머스 120802: 두 수의 합 구하기
+URL : https://school.programmers.co.kr/learn/courses/30/lessons/120802
+'''
+def solution(num1, num2):
+    answer = num1 + num2
+    return answer
