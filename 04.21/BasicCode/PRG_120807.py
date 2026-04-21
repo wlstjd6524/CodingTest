d2aa6585b5e611f165974307ffaf6da3e83f262e
wlstjd6524 <wlstjd6524@naver.com>
2026-04-21 22:25:46 +0900

    feat: PRG 120807 입문 - 숫자 비교하기

---
+++ 04.21/BasicCode/PRG_120807.py
@@ -0,0 +1,11 @@
+'''
+[프로그래머스 입문코드 다루기]
+
+프로그래머스 120807: 숫자 비교하기
+URL : https://school.programmers.co.kr/learn/courses/30/lessons/120807
+'''
+def solution(num1, num2):
+    if num1 == num2:
+        return 1
+    else:
+        return -1
