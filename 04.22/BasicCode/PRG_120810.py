e517500ad39448b7810311c685ffe22b7410c3b8
wlstjd6524 <wlstjd6524@naver.com>
2026-04-22 23:50:35 +0900

    feat: PRG 120810 입문 - 나머지 구하기

---
+++ 04.22/BasicCode/PRG_120810.py
@@ -0,0 +1,9 @@
+'''
+[프로그래머스 입문코드 다루기]
+
+프로그래머스 120810: 나머지 구하기
+URL : https://school.programmers.co.kr/learn/courses/30/lessons/120810
+'''
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
