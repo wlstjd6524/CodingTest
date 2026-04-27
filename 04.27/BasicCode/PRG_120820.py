fceb816f4111f9c4a14401ce960b727eb4a81623
wlstjd6524 <wlstjd6524@naver.com>
2026-04-27 21:33:17 +0900

    feat: PRG 120820 입문 - 나이 출력

---
+++ 04.27/BasicCode/PRG_120820.py
@@ -0,0 +1,10 @@
+'''
+[프로그래머스 입문코드 다루기]
+
+프로그래머스 120820: 나이 출력
+URL : https://school.programmers.co.kr/learn/courses/30/lessons/120820
+'''
+
+def solution(age):
+    answer = 2022 - (age - 1)
+    return answer
