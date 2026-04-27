5d64c08f298edfd5e6afa6139baca4be4c01a3af
wlstjd6524 <wlstjd6524@naver.com>
2026-04-27 21:33:33 +0900

    feat: PRG 120821 입문 - 배열 뒤집기

---
+++ 04.27/BasicCode/PRG_120821.py
@@ -0,0 +1,9 @@
+'''
+[프로그래머스 입문코드 다루기]
+
+프로그래머스 120821: 배열 뒤집기
+URL : https://school.programmers.co.kr/learn/courses/30/lessons/120821
+'''
+def solution(num_list):
+    answer = num_list[::-1]
+    return answer
