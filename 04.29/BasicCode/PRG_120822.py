66d38544a1b2067cc382dbc836aa0ae930b6ef7b
wlstjd6524 <wlstjd6524@naver.com>
2026-04-29 17:27:22 +0900

    feat: PRG 120822 입문 - 뒤집힌 문자열

---
+++ 04.29/BasicCode/PRG_120822.py
@@ -0,0 +1,9 @@
+'''
+[프로그래머스 입문코드 다루기]
+
+프로그래머스 120822: 뒤집힌 문자열
+URL : https://school.programmers.co.kr/learn/courses/30/lessons/120822
+'''
+def solution(my_string):
+    answer = my_string[::-1]
+    return answer
