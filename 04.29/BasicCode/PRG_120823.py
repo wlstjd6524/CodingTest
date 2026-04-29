49c25532d88566178423af8ea942ffeb522e2d4a
wlstjd6524 <wlstjd6524@naver.com>
2026-04-29 17:27:39 +0900

    feat: PRG 120823 입문 - 직각삼각형 출력하기

---
+++ 04.29/BasicCode/PRG_120823.py
@@ -0,0 +1,10 @@
+'''
+[프로그래머스 입문코드 다루기]
+
+프로그래머스 120823: 직각삼각형 출력하기
+URL : https://school.programmers.co.kr/learn/courses/30/lessons/120823
+'''
+n = int(input())
+
+for i in range(1, n+1):
+    print('*' * i)
