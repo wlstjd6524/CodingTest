42cd5fa3d51d6ea0af65a5afb3f143875e7093f5
wlstjd6524 <wlstjd6524@naver.com>
2026-05-08 00:56:41 +0900

    feat: PRG 120833 입문 - 배열 자르기

---
+++ 05.06/BasicCode/PRG_120833.py
@@ -0,0 +1,8 @@
+'''
+[프로그래머스 입문코드 다루기]
+
+프로그래머스 120833: 배열 자르기
+URL : https://school.programmers.co.kr/learn/courses/30/lessons/120833
+'''
+def solution(numbers, num1, num2):
+    return numbers[num1:num2+1]
